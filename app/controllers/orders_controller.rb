1499452a8e0d34bee17a2c2c755f0c359a572626
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-03-13 15:30:44 +0900

    set charge object

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,4 +11,13 @@ class OrdersController < ApplicationController
   def order_params
     params.permit(:price, :token)
   end
+
+  def pay_item
+   Payjp.api_key = "sk_test_〇〇〇〇〇〇"  # 秘密鍵を設定
+   Payjp::Charge.create(
+     amount: order_params[:price],  # 商品の値段
+     card: order_params[:token],    # カードトークン
+     currency:'jpy'                 # 通貨の種類
+   )
+  end
 end
